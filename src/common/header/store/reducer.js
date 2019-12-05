3b0942f83bf00797c1f06b141b503ab14fecf36f
Torvalds Du <torvalds@ekohe.com>
2019-12-05 21:20:35 +0800

    7-12 引入 redux-thunk来处理异步数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -2,7 +2,8 @@ import * as constants from "./constants";
 import { fromJS } from "immutable";
 
 const initialState = fromJS({
-  focused: false
+  focused: false,
+  list: []
 });
 
 export default (state = initialState, { type }) => {
