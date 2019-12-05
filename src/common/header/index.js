7cbf7e8a0e152da4a0403998832d75f3f386ef84
Torvalds Du <torvalds@ekohe.com>
2019-12-05 21:17:40 +0800

    7-12 把无状态组件转换成 class

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   HeaderWrapper,
   Logo,
@@ -18,60 +18,65 @@ import { CSSTransition } from "react-transition-group";
 import { connect } from "react-redux";
 import { actionCreators } from "../header/store";
 
-const getListArea = show => {
-  if (show) {
+class Header extends Component {
+  getListArea = show => {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+            <SearchInfoItem>教育</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      );
+    }
+  };
+  render() {
     return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一批</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>教育</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载 APP</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={this.props.focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.props.focused ? "focused" : ""}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <i className={this.props.focused ? "focused iconfont" : "iconfont"}>
+              &#xe848;
+            </i>
+            {this.getListArea(this.props.focused)}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writting">
+            <i className="iconfont">&#xe60e;</i>写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </HeaderWrapper>
     );
   }
-};
-
-const Header = props => {
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载 APP</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">
-          <i className="iconfont">&#xe636;</i>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition in={props.focused} timeout={200} classNames="slide">
-            <NavSearch
-              className={props.focused ? "focused" : ""}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <i className={props.focused ? "focused iconfont" : "iconfont"}>
-            &#xe848;
-          </i>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className="writting">
-          <i className="iconfont">&#xe60e;</i>写文章
-        </Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+}
 
 const mapStateToProps = state => {
   return {
