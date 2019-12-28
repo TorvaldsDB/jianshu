11ab520e35167bb582468e87b3f7bb3555d2c40e
Torvalds Du <torvalds@ekohe.com>
2019-12-28 12:46:28 +0800

    8-1 什么是路由, 如何在React 中使用路由功能

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./GlobalStyle";
 import { GlobalIconStyle } from "./statics/iconfont/iconfont";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -11,7 +12,19 @@ function App() {
       <GlobalStyle />
       <GlobalIconStyle />
       <Provider store={store}>
-        <Header />
+        <>
+          <Header />
+          <BrowserRouter>
+            <>
+              <Route path="/" exact render={() => <div>home</div>}></Route>
+              <Route
+                path="/detail"
+                exact
+                render={() => <div>detail</div>}
+              ></Route>
+            </>
+          </BrowserRouter>
+        </>
       </Provider>
     </React.Fragment>
   );
