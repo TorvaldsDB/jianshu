844f9a9dd8daf38e1221d386b414a3dec2e7587a
Torvalds Du <torvalds@ekohe.com>
2019-12-28 19:24:28 +0800

    8-9 抽取函数, 优化 reducer.js

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,19 +9,27 @@ const initialState = fromJS({
   showScroll: false
 });
 
+const changeHomeData = (state, payload) => {
+  return state.merge({
+    topicList: fromJS(payload.topicList),
+    articleList: fromJS(payload.articleList),
+    recommendList: fromJS(payload.recommendList)
+  });
+};
+
+const addArticleList = (state, payload) => {
+  return state.merge({
+    articleList: state.get("articleList").concat(payload.list),
+    articlePage: payload.nextPage
+  });
+};
+
 export default (state = initialState, { type, ...payload }) => {
   switch (type) {
     case constants.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: fromJS(payload.topicList),
-        articleList: fromJS(payload.articleList),
-        recommendList: fromJS(payload.recommendList)
-      });
+      return changeHomeData(state, payload);
     case constants.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get("articleList").concat(payload.list),
-        articlePage: payload.nextPage
-      });
+      return addArticleList(state, payload);
     case constants.TOGGLE_SCROLL_TOP:
       return state.set("showScroll", payload.show);
     default:
