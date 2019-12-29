5232e1527dbc314b9e803195b426a48321a6b0fc
Torvalds Du <torvalds@ekohe.com>
2019-12-29 14:23:52 +0800

    9-7 代码简单优化

--- src/pages/detail/store/actionCreators.js
+++ src/pages/detail/store/actionCreators.js
@@ -10,9 +10,14 @@ const changeDetail = (title, content) => ({
 
 export const getDetail = id => {
   return dispatch => {
-    axios.get(`/api/detail.json?id=${id}`).then(res => {
-      const result = res.data.data;
-      dispatch(changeDetail(result.title, result.content));
-    });
+    axios
+      .get(`/api/detail.json?id=${id}`)
+      .then(res => {
+        const result = res.data.data;
+        dispatch(changeDetail(result.title, result.content));
+      })
+      .catch(() => {
+        alert("登录失败, 请重试");
+      });
   };
 };
